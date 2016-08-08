5cb137decac9ee6936a5ff1f270d084f96d2e6ee
emanuel <emanuelgianico@gmail.com>
2016-08-08 16:53:23 -0300

    Support for query

--- utils.js
+++ utils.js
@@ -1,8 +1,19 @@
 module.exports.urlEncode = function urlEncode(obj) {
   var str = [];
-  for (var p in obj) {
+  for (var p in obj) {	
     if (obj.hasOwnProperty(p)) {
-      str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+		if(obj[p] instanceof Array)
+		{
+			var arrToUri = "";
+			
+			for(var i in obj[p]) {						
+				arrToUri += p + "[]" + "=" + obj[p][i] + "&";
+			}
+			
+			str.push(arrToUri.substring(0, arrToUri.length - 1));
+		}
+		else 
+			str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
     }
   }
   return str.join('&');
