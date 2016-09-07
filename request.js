4646c4f231cbf13f1145a6fad000d4dc054f0412
Jesse Wood <jess.nz@gmail.com>
2016-09-08 08:46:22 +1200

    Allow url starting with `http` to override baseUrl

--- request.js
+++ request.js
@@ -4,7 +4,11 @@ module.exports = function request(options, cb) {
   xhr.addEventListener('load', cb);
   xhr.addEventListener('error', cb);
   xhr.addEventListener('abort', cb);
-  xhr.open(options.method, options.baseUrl + options.url)
+  xhr.open(options.method, 
+    options.url.indexOf('http') > -1 
+      ? options.url
+      : options.baseUrl + options.url
+  )
   options.onRequest(xhr, options)
   return xhr
 }
